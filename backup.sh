742091bcf03f2524e00d9dcd622127ae5cdd56a4
difki <i.difki@gmail.com>
2017-10-06 06:43:32 +0000

    Adding new backup

--- backup.sh
+++ backup.sh
@@ -1,1 +1,4 @@
-mysqldump -u root barifrah1_bitahon > /home/ilyas/git/sme_shift_sql_backup/sme_db_dump_$(date +\%s); git add -A;git commit -m"Adding new backup";git ; git add -A;git commit -m"Adding new backup";git push
+mysqldump -u root barifrah1_bitahon > /home/ilyas/git/sme_shift_sql_backup/sme_db_dump_$(date +\%s); 
+git add -A
+git commit -m"Adding new backup"
+git push
